940223fa2c220fbf3d149e7c9106ce89428230ea
MeysamH97 <meysamhasanifx@gmail.com>
2024-07-03 02:40:47 +0430

    create base

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+
+  static const baseUrl = 'https://home.openweathermap.org';
+  static const apiKey1 = '0c277c6791c190f7b48b39bf3f0959df';
+
+}
---
+++ lib/features/weather__feature/data/data_source/remote/api_provider.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/core/utils/constants.dart';
+
+class ApiProvider {
+  final Dio dio = Dio();
+  var apiKey = Constants.apiKey1;
+
+  /// current weather api call
+  Future<dynamic> getCurrentWeather(cityName) async{
+    var response = await dio.get(
+      '${Constants.baseUrl}/data/2.5/weather',
+      queryParameters: {
+        'q' : cityName,
+        'appid' : apiKey,
+        'units' : 'metric',
+      },
+    );
+    return response;
+  }
+
+}
